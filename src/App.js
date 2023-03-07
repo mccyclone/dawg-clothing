9b348075110fadb111046db1247d65fd12300c2e
mccyclone <anthonylhenry@gmail.com>
2023-03-07 17:50:11 -0600

    (86) Navigation Bar Scaffolding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigatiion = () => {
+  return(
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App = () => {
   return (
-    
+
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigatiion />} >
+        <Route index={true} element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
-    
   );
 };
 
